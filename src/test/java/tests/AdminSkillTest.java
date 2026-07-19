cf0232ff76df9d908ca2e56f4bac25af8d44b5f9
dhabalekiran358-byte <dhabalekiran358@gmail.com>
2026-07-19 08:47:10 +0530

    Update AdminSkillTest.java

--- src/test/java/tests/AdminSkillTest.java
+++ src/test/java/tests/AdminSkillTest.java
@@ -1,47 +1,37 @@
 package tests;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.AdminSkillPage;
 import pages.LoginPage;
 
-@Test
 public class AdminSkillTest extends BaseTest {
 
-    private AdminSkillPage adminSkillPage;
-
-    @BeforeMethod
-    public void pageSetup() {
-    	adminSkillPage = new AdminSkillPage(driver);
-
-        String username = p.getProperty("username");
-        String password = p.getProperty("password");
+	@Test
+    public void verifyAddSkill() throws InterruptedException {
 
         LoginPage lp = new LoginPage(driver);
-        lp.login(username, password);
-    }
-    
-    
-    @Test(dataProvider = "skillData")
-    public void verifyAddSkill(String skillName, String description) {
+        AdminSkillPage admin = new AdminSkillPage(driver);
 
-        adminSkillPage.clickAdmin();
-        adminSkillPage.clickQualification();
-        adminSkillPage.clickSkills();
-        adminSkillPage.clickAddButton();
+        String uname = p.getProperty("username");
+        String pwd = p.getProperty("password");
 
-        adminSkillPage.enterName(skillName);
-        adminSkillPage.enterDescription(description);
-    }
+        lp.login(uname, pwd);
 
-    @DataProvider(name = "skillData")
-    public Object[][] skillData() {
-        return new Object[][] {
-            {"Java", "Java Programming"},
-            {"Python", "Python Programming"}
-        };
+        Assert.assertTrue(lp.isDashboardDisplayed());
+
+        String skillName = " Java Selenium";
+        String description = "Shreesha Surwase";
+
+        admin.addSkill(skillName, description);
+
+        Assert.assertTrue(admin.isSkillAdded(skillName));
+
+        Reporter.log("Skill Added Successfully", true);
+
+        Thread.sleep(3000);
     }
 }
