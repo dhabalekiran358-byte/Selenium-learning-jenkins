0883588e71a0bba9533ad41cbb28d31e0914e480
dhabalekiran358-byte <dhabalekiran358@gmail.com>
2026-07-19 08:45:44 +0530

    Update AdminSkillPage.java

--- src/test/java/pages/AdminSkillPage.java
+++ src/test/java/pages/AdminSkillPage.java
@@ -1,55 +1,83 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AdminSkillPage {
 
     WebDriver driver;
-
+   
     public AdminSkillPage(WebDriver driver) {
         this.driver = driver;
+      
     }
 
-    By adminMenu = By.xpath("//span[normalize-space()='Admin']");
-    By qualification = By.xpath("//span[normalize-space()='Qualifications']");
-    By skills = By.xpath("//a[normalize-space()='Skills']");
-    By addButton = By.xpath("//button[contains(.,'Add')]");
-    By name = By.xpath("//label[normalize-space()='Name']/ancestor::div[contains(@class,'oxd-input-group')]//input");
-    By description = By.xpath("//textarea");
-    By submit = By.xpath("//button[@type='submit']");
+    // Locators
+    By adminMenu = By.xpath("//span[text()='Admin']");
+    By topBar = By.xpath("//header");
+    By qualificationDropDown = By.xpath("//span[text()='Qualifications ']");
+    By skillsMenu = By.xpath("//a [text()='Skills']");                     
+    By addButton = By.xpath("//button[text()=' Add ']");
+    By skillName = By.xpath("(//input[@class='oxd-input oxd-input--active'])[2]");                 //Finds the input field where you type the skill name.
+    By description = By.tagName("textarea");
+    By saveButton = By.xpath("//button[@type='submit']");
+
+    public void clickAdmin() throws InterruptedException {
+        driver.findElement(adminMenu).click();
+        Thread.sleep(5000); 
+    }
+
+    public void clickQualifications() throws InterruptedException {
+        driver.findElement(qualificationDropDown).click();
+        Thread.sleep(5000); 
+    }
+
+    public void clickSkills() throws InterruptedException {
+        driver.findElement(skillsMenu).click();
+        Thread.sleep(5000); 
+    }
+
+    public void clickAdd() throws InterruptedException {
+        driver.findElement(addButton).click();
+        Thread.sleep(5000); 
+    }
+
+    public void enterSkillName(String skill) {
+        driver.findElement(skillName).sendKeys(skill);
+    }
+
+    public void enterDescription(String desc) {
+        driver.findElement(description).sendKeys(desc);
+    }
+
+    public void clickSave() throws InterruptedException {
+        driver.findElement(saveButton).click();
+        Thread.sleep(5000); 
+    }
+
+    public void addSkill(String skill, String desc) throws InterruptedException {
+        clickAdmin();
+        clickQualifications();
+        clickSkills();
+        clickAdd();
+        enterSkillName(skill);
+        enterDescription(desc);
+        clickSave();
+    }
+
+    public boolean isSkillAdded(String skill) {
+        By skillText = By.xpath("//div[text()='" + skill + "']");                   //This is used after saving to verify that the skill appears in the Skills list.
+        return driver.findElements(skillText).size() > 0;
+    }
+}
 
     
 
-	public void clickAdmin() {
-		// TODO Auto-generated method stub
-		driver.findElement(adminMenu).click();
-	}
-
-	public void clickQualification() {
-		// TODO Auto-generated method stub
-		driver.findElement(qualification).click();
-	}
-
-	public void clickSkills() {
-		// TODO Auto-generated method stub
-		driver.findElement(skills).click();
-	}
-
-	public void clickAddButton() {
-		// TODO Auto-generated method stub
-		driver.findElement(addButton).click();
-	}
 	
-	public void enterName(String skillName) {
-        driver.findElement(name).sendKeys(skillName);
-    }
 
-    public void enterDescription(String descriptionText) {
-        driver.findElement(description).sendKeys(descriptionText);
-    }
-
-    public void clickSubmit() {
-        driver.findElement(submit).click();
-    }
-}
+	
+  
